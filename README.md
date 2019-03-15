1cc3b2332ec72c3c864ffd8c2748ba7bcdb4630e
themindstorm <parthkabra@Parths-MacBook-Air.local>
2019-03-16 00:24:25 +0800

    Added structure

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
-# nested_scroll_views
+# Flutter Autohiding application bar with tabs
 
-A new Flutter project.
+- Flutter app bar + tab bar with hide on scroll functionality
 
-## Getting Started
+## Structure
+- MaterialApp
+  - DefaultTabController
+    - Scaffold
+      - NestedScrollView
+        - SliverAppBar
+          - TabBar
+        - TabBarView
 
-This project is a starting point for a Flutter application.
+## Built with
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
-
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.io/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+- `DefaultTabController`
+- `NestedScrollView`
+- `SliverAppBar`
+- `TabBar` + `TabBarView`
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final List<String> listItems = [];
+
+  @override
+  Widget build(BuildContext context) {
+    for (var i = 0; i < 500; i++) {
+      listItems.add('Item ${i}');
+    }
+
+    return new MaterialApp(
+      home: new DefaultTabController(
+        length: 2,
+        child: new Scaffold(
+          body: new NestedScrollView(
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                new SliverAppBar(
+                  title: Text("Whatsapp-like beahaviour"),
+                  floating: true,
+                  pinned: true,
+                  snap: true,
+                  bottom: new TabBar(
+                    tabs: [
+                      new Tab(text: 'tab1'),
+                      new Tab(text: 'tab2'),
+                    ],
+                  ),
+                ),
+              ];
+            },
+            body: new TabBarView(
+              children: [
+                ListView.builder(
+                  itemCount: listItems.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.blueGrey,
+                      ),
+                      title: Text(listItems[index]),
+                    );
+                  },
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    "This is the second screen in the app. As you can see, the top of this text gets hidden",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w900,
+                      fontSize: 30.0
+                    ),
+                  )
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
